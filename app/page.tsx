3d8abd9a4fd3cafb5918376f8f84c0d6617f0a8c
Cotne Basiashvili <basiashvilic@gmail.com>
2024-01-11 21:40:14 +0400

    added new font family and some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 export default function Home() {
   return (
     <main className="">
-      <section className="flex flex-col gap-28">
+      <section className="flex flex-col gap-28 text-right items-end p-[8%] h-[90vh]">
         <div>
-          <p className="text-lg font-light leading-6">Hello all. I am</p>
-          <h1 className="font-light text-[4vw]">Cotne Basiashvili</h1>
-          <h2 className="text-[#4d5bce] font-bold text-[32px]">
-            Front-End Web Developer
+          <p className="font-light leading-6 Caveat text-7xl mb-8">"HELLO"</p>
+          <h1 className="font-light text-8xl text-[#B336FF]">
+            <span className="text-white">i am </span>Cotne Basiashvili
+          </h1>
+          <h2 className="text-xl font-normal w-[696px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            sollicitudin egestas neque nec volutpat. Cras rhoncus magna id
+            tellus sodales viverra eu eu nisi. Nam venenatis mattis dictum.
+            Praesent venenatis leo dui, sed auctor eros posuere ac. Etiam mauris
+            augue, laoreet quis ex ut, sollicitudin cursus orci. Donec in est
+            vitae sem scelerisque tempor sit amet eu sem.
           </h2>
         </div>
         <div>
