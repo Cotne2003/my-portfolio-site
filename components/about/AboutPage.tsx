33f3cf858540c19356b53f09fa41fdfa199f719c
Cotne Basiashvili <basiashvilic@gmail.com>
2024-01-15 02:04:56 +0400

    added tab functions

--- components/about/AboutPage.tsx
+++ components/about/AboutPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, MouseEventHandler } from "react";
 import FirstSection from "./FirstSection";
 
 const AboutPage = () => {
@@ -9,16 +9,41 @@ const AboutPage = () => {
   const profTxt =
     "As a junior front-end web developer, I am committed to an ongoing journey of skill enhancement and embracing emerging technologies. While my foray into the expansive realm of tech occurred just a few months ago, I've swiftly demonstrated remarkable adaptability and a keen ability to learn flexibly. Although my solid foundation in IT from Georgian Technology University provided a robust base, my appetite for more adventurous learning experiences prompted me to seek new horizons. In pursuit of these endeavors, I have recently joined a dynamic team, where I look forward to collaborating on exciting projects that not only challenge me but also contribute to my growth as a developer. This decision reflects my eagerness to immerse myself in a stimulating environment, where the synergy of teamwork and innovative projects fosters continuous learning and professional development.";
 
+  // TEXTS ///////////////////////
   const [pers, setPers] = useState(true);
   const [prof, setProf] = useState(false);
 
-  const persHandler = () => {
+  const persHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    e?.stopPropagation();
     setPers(true);
+    setPersTab(true);
     setProf(false);
   };
   const profHandler = () => {
     setPers(false);
+    setProfTab(true);
     setProf(true);
+  };
+
+  // TABS ////////////////////////
+  const [persTab, setPersTab] = useState(true);
+  const [profTab, setProfTab] = useState(false);
+
+  const persTabHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (profTab) {
+      e.stopPropagation();
+      setPersTab(false);
+      setProf(true);
+      setPers(false);
+    }
+  };
+  const profTabHandler: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (persTab) {
+      e.stopPropagation();
+      setProfTab(false);
+      setPers(true);
+      setProf(false);
+    }
   };
 
   return (
@@ -31,26 +56,44 @@ const AboutPage = () => {
       />
       <section className="w-[40%] mt-[-5px]">
         <div className="flex gap-10">
-          <div className="cursor-pointer">
-            <h2
-              className={`text-2xl rounded-lg py-2 pl-4 w-[210px] ${
-                pers ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
-              }`}
-              onClick={persHandler}
-            >
-              // info <span className="text-[#607b96]">/ personal</span>
-            </h2>
-          </div>
-          <div className="cursor-pointer">
-            <h2
-              className={`text-2xl rounded-lg py-2 pl-4 w-[240px] ${
-                prof ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
-              }`}
-              onClick={profHandler}
-            >
-              // info <span className="text-[#607b96]">/ PROFFESIONAL</span>
-            </h2>
-          </div>
+          {persTab && (
+            <div className="cursor-pointer">
+              <h2
+                className={`text-2xl rounded-lg py-2 pl-4 w-[210px] flex justify-between pr-4  ${
+                  pers ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
+                }`}
+                onClick={persHandler}
+              >
+                <div>
+                  // info <span className="text-[#607b96]">/ personal</span>
+                </div>
+                <span
+                  className="Caveat text-gray-400 hover:text-white"
+                  onClick={persTabHandler}
+                >
+                  X
+                </span>
+              </h2>
+            </div>
+          )}
+          {profTab && (
+            <div className="cursor-pointer">
+              <h2
+                className={`text-2xl rounded-lg py-2 pl-4 w-[260px] flex justify-between pr-4 ${
+                  prof ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
+                }`}
+                onClick={profHandler}
+              >
+                // info <span className="text-[#607b96]">/ PROFFESIONAL</span>
+                <span
+                  className="Caveat text-gray-400 hover:text-white"
+                  onClick={profTabHandler}
+                >
+                  X
+                </span>
+              </h2>
+            </div>
+          )}
         </div>
         <p className="mt-[37px] text-[#8bb4dd] text-xl font-sans">
           {pers ? persTxt : prof ? profTxt : ""}
--- components/about/FirstSection.tsx
+++ components/about/FirstSection.tsx
@@ -1,79 +1,97 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import arrow from "/public/logos/arrow.png";
 import email from "/public/logos/Email.svg";
 import telephone from "/public/logos/telephone.png";
 
 type Props = {
-  persHandler: () => void;
+  persHandler: MouseEventHandler<HTMLDivElement>;
   pers: boolean;
   profHandler: () => void;
   prof: boolean;
 };
 
 const FirstSection = ({ persHandler, pers, profHandler, prof }: Props) => {
+  const [info, setInfo] = useState(true);
+  const [contacts, setContacts] = useState(false);
+  const infoHandler = () => {
+    setInfo(!info);
+  };
+  const contactsHandler = () => {
+    setContacts(!contacts);
+  };
   console.log("test");
   return (
-    <section className="">
+    <section className="w-[220px]">
       <div className="">
         <div className=" w-[170px]">
           <h2 className="text-5xl">about-me</h2>
         </div>
         <div className="mt-[40px]">
-          <div className="rounded-lg py-1 pl-4 bg-[#1e2d3d] text-2xl Caveat uppercase flex items-center gap-1">
-            <span>
+          <div
+            className="rounded-lg py-1 pl-4 bg-[#1e2d3d] text-2xl Caveat uppercase flex items-center gap-1 cursor-pointer"
+            onClick={infoHandler}
+          >
+            <span className={`${info ? "rotate-90" : "rotate-0"}`}>
               <img src={arrow.src} width={10} alt="" />
             </span>{" "}
             Info
           </div>
-          <div className="mt-3">
-            <div
-              className={`pl-5 flex items-center gap-3 Caveat uppercase py-1 ${
-                pers ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
-              } rounded-lg cursor-pointer`}
-              onClick={persHandler}
-            >
-              <span className={`${pers ? "rotate-90" : "rotate-0"}`}>
-                <img src={arrow.src} width={10} alt="" />
-              </span>{" "}
-              personal
+          {info && (
+            <div className="mt-3">
+              <div
+                className={`pl-5 flex items-center gap-3 Caveat uppercase py-1 ${
+                  pers ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
+                } rounded-lg cursor-pointer`}
+                onClick={persHandler}
+              >
+                <span className={`${pers ? "rotate-90" : "rotate-0"}`}>
+                  <img src={arrow.src} width={10} alt="" />
+                </span>{" "}
+                personal
+              </div>
+              <div
+                className={`pl-5 flex items-center gap-3 Caveat uppercase py-1 cursor-pointer rounded-lg mt-1 ${
+                  prof ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
+                }`}
+                onClick={profHandler}
+              >
+                <span className={`${prof ? "rotate-90" : "rotate-0"}`}>
+                  <img src={arrow.src} width={10} alt="" />
+                </span>{" "}
+                proffesional
+              </div>
             </div>
-            <div
-              className={`pl-5 flex items-center gap-3 Caveat uppercase py-1 cursor-pointer rounded-lg mt-1 ${
-                prof ? "bg-[#5e5e5e7a]" : "hover:bg-[#5e5e5e7a]"
-              }`}
-              onClick={profHandler}
-            >
-              <span className={`${prof ? "rotate-90" : "rotate-0"}`}>
-                <img src={arrow.src} width={10} alt="" />
-              </span>{" "}
-              proffesional
-            </div>
-          </div>
+          )}
         </div>
       </div>
 
       <div className="">
         <div className="mt-[20px]">
-          <div className="rounded-lg py-1 pl-4 bg-[#1e2d3d] text-2xl Caveat uppercase flex items-center gap-1">
-            <span>
+          <div
+            className="rounded-lg py-1 pl-4 bg-[#1e2d3d] text-2xl Caveat uppercase flex items-center gap-1 cursor-pointer"
+            onClick={contactsHandler}
+          >
+            <span className={`${contacts ? "rotate-90" : "rotate-0"}`}>
               <img src={arrow.src} width={10} alt="" />
             </span>{" "}
             contacts
           </div>
-          <div className="mt-3">
-            <div className="pl-5 flex gap-3 items-center font-sans py-1 hover:opacity-50 cursor-pointer">
-              <span>
-                <img src={email.src} width={15} alt="" />
-              </span>
-              basiashvilic@gmail.com
+          {contacts && (
+            <div className="mt-3">
+              <div className="pl-5 flex gap-3 items-center font-sans py-1 hover:opacity-50 cursor-pointer">
+                <span>
+                  <img src={email.src} width={15} alt="" />
+                </span>
+                basiashvilic@gmail.com
+              </div>
+              <div className="pl-5 flex gap-3 items-center font-sans py-1 hover:opacity-50 cursor-pointer">
+                <span>
+                  <img src={telephone.src} width={15} alt="" />
+                </span>
+                +(995) 568 864 004
+              </div>
             </div>
-            <div className="pl-5 flex gap-3 items-center font-sans py-1 hover:opacity-50 cursor-pointer">
-              <span>
-                <img src={telephone.src} width={15} alt="" />
-              </span>
-              +(995) 568 864 004
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </section>
