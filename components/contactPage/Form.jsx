c41cd185aa582eeb3a1b5453104a5655285de682
Cotne Basiashvili <basiashvilic@gmail.com>
2024-04-18 06:57:53 +0400

    added some logics on contacp page

--- components/contactPage/Form.jsx
+++ components/contactPage/Form.jsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
+import { toast } from "react-toastify";
 
 const From = () => {
   const form = useRef();
 
+  const [wait, setWait] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
+    setWait(true);
 
     emailjs
       .sendForm(
@@ -19,13 +23,20 @@ const From = () => {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          console.log(result);
+          setWait(false);
+          if (result.status === 200) {
+            toast.success("Message sent successfully");
+          } else {
+            toast.error("Unexpected error occurred while sending message");
+          }
         },
         (error) => {
           console.log(error.text);
         }
       );
   };
+
   return (
     <motion.section
       className="w-[770px] mt-[65px]"
@@ -38,6 +49,8 @@ const From = () => {
           <div className="flex flex-col">
             <label className="font-sans text-[#607b96]">Name</label>
             <input
+              required={true}
+              autoComplete="off"
               type="text"
               name="user_name"
               className="bg-[#011221] border-solid border-[#1e2d3d] border-[1px] rounded-md outline-none focus:border font-sans pl-[15px] py-[10px] focus:border-[#48627e]"
@@ -47,6 +60,8 @@ const From = () => {
             <label className="font-sans text-[#607b96]">Email</label>
             <input
               type="email"
+              autoComplete="off"
+              required={true}
               name="user_email"
               className="bg-[#011221] border-solid border-[#1e2d3d] border-[1px] rounded-md font-sans pl-[15px] py-[10px] focus:border-[#48627e] outline-none"
             />
@@ -54,15 +69,16 @@ const From = () => {
           <div className="flex flex-col">
             <label className="font-sans text-[#607b96]">Message</label>
             <textarea
+              required={true}
               name="message"
               className="bg-[#011221] border-solid border-[#1e2d3d] border-[1px] rounded-md font-sans pl-[15px] h-[200px] py-[10px] focus:border-[#48627e] outline-none"
             />
           </div>
           <button
             type="submit"
-            className="font-sans bg-[#1e2d3d] w-[100px] rounded-md py-2"
+            className="font-sans bg-[#1e2d3d] w-[100px] rounded-md py-2 uppercase font-medium hover:scale-105 ease-out duration-300 active:scale-95"
           >
-            send
+            {wait ? "wait" : "send"}
           </button>
         </form>
       </div>
